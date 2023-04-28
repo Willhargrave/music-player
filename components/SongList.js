a3992a7999c6b3804b1c26cce7dc0b0bbcff00ef
will <willhargrave@hotmail.co.uk>
2023-04-28 13:30:03 +0900

    songlist styles

--- components/SongList.js
+++ components/SongList.js
@@ -1,6 +1,7 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
+import styles from './styles/songlist.style'
 const SongList = ({songs, onSongPress}) => {
     const router = useRouter();
     return (
@@ -10,9 +11,11 @@ const SongList = ({songs, onSongPress}) => {
             key={song.key}
             style={styles.container}
             onPress={() => onSongPress(song)}
-            ></TouchableOpacity>
+            >
+                <Text style={styles.title}>{song.title}</Text>
+                <Text style={styles.artist}>{song.artist}</Text>
+            </TouchableOpacity>
         })}
-        <Text>SongList</Text>
         </View>
     );
 }
---
+++ components/styles/songlist.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    songContainer: {
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderBottomWidth: 1,
+      borderBottomColor: "#ddd",
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    artist: {
+      fontSize: 14,
+    },
+  });
+
+  export default styles;
