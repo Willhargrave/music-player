cc87b651e1b056c2ca63bd24431f269f88854726
will <willhargrave@hotmail.co.uk>
2023-04-28 12:07:23 +0900

    song-styling

--- components/Song.js
+++ components/Song.js
@@ -2,15 +2,17 @@ import {View, ScrollView, SafeAreaView, Text, Image} from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import styles from './styles/song.style';
 const Song = ({title, artist, album, duration}) => {
     const router = useRouter();
     return (
-          <View>
-            <Image source={require('../assets/Liquidswords.png')} />
-            <Text>{title}</Text>
-            <Text>{artist}</Text>
-            <Text>{album}</Text>
-            <Text>{duration}</Text>
+          <View style={styles.container}>
+            <Image style={styles.image}
+             source={require('../assets/Liquidswords.png')} />
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.artist}>{artist}</Text>
+            <Text style={styles.album}>{album}</Text>
+            <Text style={styles.duration}>{duration}</Text>
           </View>
     );
 }
