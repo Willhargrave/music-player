8e3e84deeb0184230b280a6f612bf1b4f8a71f30
will <willhargrave@hotmail.co.uk>
2023-04-28 12:55:38 +0900

    added songs

---
+++ components/AllSongs.js
@@ -0,0 +1,32 @@
+const Songs = [
+    {
+      key: "1",
+      artist: "Carly Rae Jepsen",
+      title: "Western Wind",
+      album: "The Loneliest Time",
+      duration: "3 minutes 46 seconds",  
+      audio: require("./assets/audio/WesternWind.mp3"),
+      image: require("./assets/images/TheLoneliestTime.webp"),
+    },
+    {
+        key: "2",
+        artist: "GZA",
+        title: "Shadowboxin'",
+        album: "Liquid Swords",
+        duration: "3 minutes 30 seconds",  
+        audio: require("./assets/audio/Shadowboxin.mp3"),
+        image: require("./assets/images/Liquidswords1995.png"),
+      },
+      {
+        key: "3",
+        artist: "Sakanaction",
+        title: "わすれない",
+        album: "834.194",
+        duration: "4 minutes 8 seconds",  
+        audio: require("./assets/audio/わすれない.mp3"),
+        image: require("./assets/images/834194.jpg"),
+      }
+    // Add more songs here
+  ];
+
+  export default Songs;
