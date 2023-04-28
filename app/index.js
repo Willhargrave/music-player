7c6dfbebd8c17a7acdd4554f8fc92fe51fa6c979
will <willhargrave@hotmail.co.uk>
2023-04-28 16:03:32 +0900

    handleAudioPress

--- app/index.js
+++ app/index.js
@@ -3,13 +3,58 @@ import { Stack, useRouter } from 'expo-router';
 import { Player, AudioControls, AudioVisualization, Song, SongList, StatusBar, Playlist} from '../components';
 import Songs from '../components/AllSongs';
 import { useState } from 'react';
-
+import {Audio} from 'expo-av';
 const Home = () => {
     const router = useRouter();
     const [selectedSong, setSelectedSong] = useState(null);
-    const handleSongPress = (song) => {
+    const [sound, setSound] = useState(null);
+
+    const handleSongPress = async (song) => {
         setSelectedSong(song);
+        if (sound) {
+            await sound.unloadAsync();
+            setSound(null);
+        }
+    };
+
+    const handleAudioPress = async (action) => {
+         try {
+            if(!sound) {
+                const { sound: newSound } = await Audio.Sound.createAsync(selectedSong.audio);
+                setSound(newSound);
+                await newSound.playAsync();
+            } else {
+                switch (action) {
+                    case 'play' :  
+                        await sound.playAsync();
+                        break;
+                    case 'pause' :
+                        await sound.pauseAsync();
+                        break;
+                    case 'previous' :
+                        await sound.unloadAsync();
+                        const previousIndex = Songs.findIndex((song) => song.key === selectedSong.key) - 1;   
+                        const previousSong = Songs[previousIndex >= 0 ? previousIndex : Songs.length - 1];
+                        setSelectedSong(previousSong);
+                        const { sound: previousSound} = await Audio.Sound.createAsync(previousSong.audio);
+                        setSound(previousSound);
+                        await previousSound.playAsync();
+                        break;
+                    case 'next' :
+                        await sound.unloadAsync();
+                        const nextIndex = Songs.findIndex((song) => song.key === selectedSong.key) + 1;   
+                        const nextSong = Songs[nextIndex <= Songs.length - 1 ? nextIndex : 0];
+                        setSelectedSong(nextSong);
+                        const { sound: nextSound} = await Audio.Sound.createAsync(nextSong.audio);
+                        setSound(nextSound);
+                        await nextSound.playAsync();
+                        break;
+                    }
+         }
+    } catch (error) {
+        console.warn(error)
     }
+};
     return (
         <SafeAreaView
         style={{
@@ -18,6 +63,7 @@ const Home = () => {
         <ScrollView showsVerticalScrollIndicator={false}>
             <View>
                 {selectedSong ? (
+                    <>
                     <Song 
                     title={selectedSong.title}
                     artist={selectedSong.artist}
@@ -25,11 +71,13 @@ const Home = () => {
                     duration={selectedSong.duration}
                     audio={selectedSong.audio}
                     image={selectedSong.image}/>
-                ) : (
+                <AudioControls onAudioPress={handleAudioPress}/>
+                </> ) 
+                 : (
                     <SongList songs={Songs} onSongPress={handleSongPress} />
                 )}
                 <AudioVisualization/>
-                <AudioControls />
+                
             </View>
         </ScrollView>
         </SafeAreaView>
