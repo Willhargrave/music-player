bed7da7e13d64d3bc15ce17a1818ea0eb6fea388
will <willhargrave@hotmail.co.uk>
2023-04-28 15:17:41 +0900

    Async functions

--- components/AudioControls.js
+++ components/AudioControls.js
@@ -1,7 +1,52 @@
-import {View, Button, SliderBase, Text} from 'react-native';
-
+import {View, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Audio} from 'expo-av';
 import style from './styles/Audiocontrols.style';
 const AudioControls = () => {
+const [sound, setSound] = useState(null);
+const [isplaying, setIsPlaying] = useState(false);
+async function playSound() {
+    try {
+        const {sound} = await Audio.Sound.createAsync(audio {
+            shouldPlay: true,
+            onPlaybackStatusUpdate: onPlatbackStatusUpdate,
+        });
+        setSound(sound)
+        setIsPlaying(true);
+    } catch (error) {
+        console.log("Error playing sound: ", error)
+    }
+}
+async function pauseSound() {
+    if (sound) {
+        await sound.pauseAsync();
+        setIsPlaying(false);
+    }
+}
+async function stopSound() {
+    if (sound) {
+        await sound.stopAsync();
+        setIsPlaying(false);
+    }
+}
+async function skipForward() {
+    if (sound) {
+        await sound.playFromPositionAsync(sound.positionMillis + 10000);
+    }
+}
+async function skipBackward() {
+    if (sound) {
+        await sound.playFromPositionAsync(sound.positionMillis - 10000);
+    }
+}
+
+useEffect(() => {
+    return sound
+    ? () => {
+        sound.unloadAsync();
+    }
+    : undefined;
+}, [sound]);
 
     return (
         <View>
