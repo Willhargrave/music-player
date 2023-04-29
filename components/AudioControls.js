cec428ccb11cf8f0ef239b326566ac5ec226a6d4
will <willhargrave@hotmail.co.uk>
2023-04-29 17:37:02 +0900

    updated AudioControls

--- components/AudioControls.js
+++ components/AudioControls.js
@@ -2,7 +2,7 @@ import {View, TouchableOpacity, Image} from 'react-native';
 import React, {useState, useEffect, useRef} from 'react';
 import {Audio} from 'expo-av';
 import styles from './styles/Audiocontrols.style';
-import AudioSlider from './Slider';
+import Slider from '@react-native-community/slider';
 
 const AudioControls = ({audio, onAudioPress, onSkipNext}) => {
     const [sound, setSound] = useState(null);
@@ -12,35 +12,34 @@ const AudioControls = ({audio, onAudioPress, onSkipNext}) => {
     const soundRef = useRef(null);
     const [sliderValue, setSliderValue] = useState(0);
 
-
     const onPlaybackStatusUpdate = (status) => {
-      if (status.isLoaded) {
-        setIsPlaying(status.isPlaying);
-        setDurationMillis(status.durationMillis);
-        setPositionMillis(status.positionMillis);
-        setSliderValue(status.positionMillis / status.durationMillis);
-      }
+        if (status.isLoaded) {
+            setIsPlaying(status.isPlaying);
+            setDurationMillis(status.durationMillis);
+            setPositionMillis(status.positionMillis);
+            setSliderValue(status.positionMillis / status.durationMillis);
+        }
     };
-  
+
     const playSound = async () => {
-      try {
-        const { sound } = await Audio.Sound.createAsync(audio, {
-          shouldPlay: true,
-          onPlaybackStatusUpdate: onPlaybackStatusUpdate,
-        });
-        setSound(sound);
-        soundRef.current = sound;
-      } catch (error) {
-        console.log("Error playing sound: ", error);
-      }
+        try {
+            const { sound } = await Audio.Sound.createAsync(audio, {
+                shouldPlay: true,
+                onPlaybackStatusUpdate: onPlaybackStatusUpdate,
+            });
+            setSound(sound);
+            soundRef.current = sound;
+        } catch (error) {
+            console.log("Error playing sound: ", error);
+        }
     };
-  
+
     const pauseSound = async () => {
         if (sound && sound.getStatusAsync().isLoaded) {
-          await sound.pauseAsync();
-          setIsPlaying(false);
+            await sound.pauseAsync();
+            setIsPlaying(false);
         }
-      };
+    };
 
     const handleSliderValueChange = (value) => {
         setSliderValue(value);
@@ -49,40 +48,37 @@ const AudioControls = ({audio, onAudioPress, onSkipNext}) => {
             soundRef.current.setPositionAsync(position)
         }
     }
-     
+    
     const handleSkipNext = () => {
-      if (sound) {
-        sound.unloadAsync();
-      }
-      onSkipNext();
+        if (sound) {
+            sound.unloadAsync();
+        }
+        onSkipNext();
     };
-  
-    useEffect(() => {
-      return soundRef.current ? () => {
-        soundRef.current.unloadAsync();
-      } : undefined;
-    }, []);
 
+    useEffect(() => {
+        return soundRef.current ? () => {
+            soundRef.current.unloadAsync();
+        } : undefined;
+    }, []);
 
     return (
         <View style={styles.container}>
-      <TouchableOpacity onPress={pauseSound}>
-        <Image style={styles.controlImage} source={isPlaying ? require('./assets/images/pause.png') : require('./assets/images/play.webp')}/>
-      </TouchableOpacity>
-      <AudioSlider
-        value={sliderValue}
-        onValueChange={handleSliderValueChange}
-        minimumValue={0}
-        maximumValue={1}
-        minimumTrackTintColor="#FFFFFF"
-        maximumTrackTintColor="#000000"
-        thumbTintColor="#FFFFFF"
-      />
-      <TouchableOpacity onPress={handleSkipNext}>
-        <Image style={styles.controlImage} source={require('./assets/images/skip.png')}/>
-      </TouchableOpacity>
-    </View>
-  );
+            <TouchableOpacity onPress={pauseSound}>
+                <Image style={styles.controlImage} source={isPlaying ? require('./assets/images/pause.png') : require('./assets/images/play.webp')}/>
+            </TouchableOpacity>
+            <Slider
+                style={styles.slider}
+                minimumValue={0}
+                maximumValue={1}
+                value={sliderValue}
+                onValueChange={handleSliderValueChange}
+            />
+            <TouchableOpacity onPress={handleSkipNext}>
+                <Image style={styles.controlImage} source={require('./assets/images/skip.png')}/>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 export default AudioControls;
