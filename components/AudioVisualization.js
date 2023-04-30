eda2f7071e33f5739a61bd2e111c583acb4a5dae
will <willhargrave@hotmail.co.uk>
2023-04-30 15:48:18 +0900

    Canvas

--- components/AudioVisualization.js
+++ components/AudioVisualization.js
@@ -3,8 +3,35 @@ import { Audio } from 'expo-av';
 import {View, Stylesheet} from 'react-native';
 
 
-const AudioVisualization = () => {
-    const router = useRouter();
+const AudioVisualization = ({sound, playbackPosition, height}) => {
+   const canvasRef = useRef(null);
+
+   useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const width = canvas.width;
+    const barWidth = 5;
+
+    const renderFrame = async () => { 
+        const status = await sound.getStatusAsync();
+        const pos = status.positionMillis || playbackPosition || 0;
+        const duration = status.durationMillis || 0;
+        const progress = pos / duration;
+        const barCount = Math.floor(width / barWidth);
+        const barPadding = (width - barCount * barwidth) / (barCount - 1);
+        context.clearRect(0, 0, width, height);
+
+        for (let i = 0; i < barCount; i++) { 
+            const barX = i * (barWidth + barPadding);
+            const barHeight = Math.floor(Math.random() * height);
+            context.fillRect(barX, height - barHeight, barWidth, barHeight);
+        }
+
+        context.fillStyle = '#FFF';
+        context.fillRect(0, height - 2, width, 2);
+        context.fillRect(0, height - 2, width * progress, 2);
+    }
+   })}
     return (
         <View>
         <Text>AudioVisualization</Text>
