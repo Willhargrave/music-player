b85396620eea1736788a99b3a88943345e25108b
will <willhargrave@hotmail.co.uk>
2023-05-01 09:27:16 +0900

    Canvas.js

---
+++ components/Canvas.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Canvas = React.forwardRef((props, ref) => {
+  return (
+    <View style={styles.container}>
+      <Canvas ref={ref} />
+    </View>
+  );
+});
+
+export default Canvas;
