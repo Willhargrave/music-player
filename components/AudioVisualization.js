fa6a0d36d740545c8cac548b484dc8008e87d215
will <willhargrave@hotmail.co.uk>
2023-05-02 09:48:18 +0900

    null bug

--- components/AudioVisualization.js
+++ components/AudioVisualization.js
@@ -1,36 +1,52 @@
 import React, { useRef, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Canvas from 'react-native-canvas';
+
 const AudioVisualization = ({ audioRef }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    const analyser = audioRef.current.context.createAnalyser();
-    const source = audioRef.current.getMediaElementSource();
+    let analyser, source;
 
-    source.connect(analyser);
-    analyser.connect(audioRef.current.context.destination);
+    const setup = async () => {
+      analyser = audioRef.current.context.createAnalyser();
+      source = audioRef.current.getMediaElementSource();
 
-    const render = () => {
-      const width = canvas.width;
-      const height = canvas.height;
-      const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-      analyser.getByteFrequencyData(frequencyData);
-      ctx.clearRect(0, 0, width, height);
-      ctx.fillStyle = '#00CCFF';
-      const barWidth = (width / frequencyData.length) * 2.5;
-      let x = 0;
-      for (let i = 0; i < frequencyData.length; i++) {
-        const barHeight = (frequencyData[i] / 255) * height * 0.7;
-        ctx.fillRect(x, height - barHeight, barWidth, barHeight);
-        x += barWidth + 1;
-      }
-      requestAnimationFrame(render);
+      source.connect(analyser);
+      analyser.connect(audioRef.current.context.destination);
+
+      const render = () => {
+        const width = canvas.width;
+        const height = canvas.height;
+        const frequencyData = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(frequencyData);
+        ctx.clearRect(0, 0, width, height);
+        ctx.fillStyle = '#00CCFF';
+        const barWidth = (width / frequencyData.length) * 2.5;
+        let x = 0;
+        for (let i = 0; i < frequencyData.length; i++) {
+          const barHeight = (frequencyData[i] / 255) * height * 0.7;
+          ctx.fillRect(x, height - barHeight, barWidth, barHeight);
+          x += barWidth + 1;
+        }
+        requestAnimationFrame(render);
+      };
+
+      render();
     };
 
-    render();
+    if (audioRef.current) {
+      setup();
+    }
+
+    return () => {
+      if (source) {
+        source.disconnect(analyser);
+        analyser.disconnect(audioRef.current.context.destination);
+      }
+    };
   }, [audioRef, canvasRef]);
 
   return (
